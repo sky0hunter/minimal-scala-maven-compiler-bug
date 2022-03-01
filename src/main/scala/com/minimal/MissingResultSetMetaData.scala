047fedfa5f1fdb9750e8b1ff7b02c3f23d0eea05
Josua Riha <josua.riha@channelpilot.com>
2022-03-01 12:26:35 +0100

    Use even less code.

--- src/main/scala/com/minimal/MissingResultSetMetaData.scala
+++ src/main/scala/com/minimal/MissingResultSetMetaData.scala
@@ -1,16 +1,13 @@
 package com.minimal
 
-import akka.stream.alpakka.slick.scaladsl.{Slick, SlickSession}
 import slick.jdbc.MySQLProfile.api._
 
 import scala.util.Try
 class MissingResultSetMetaData {
 
-    implicit val slickSession = SlickSession.forConfig("mysql")
-
-     def findCountryByIso3Code()  = {
-        Try {
-            Slick.source(sql"select 1".as[Int])
-        }
+  def selectOne()  = {
+    Try {
+      sql"select 1".as[Int]
     }
+  }
 }
